2dc2d0ff309bff9a11156d8b5557e4f00c0cfba3
astiz29 <anthonyduranton@laposte.net>
2015-09-21 09:49:18 +0200

    Ajout d'un footer contenant les mentions légales

--- index.php
+++ index.php
@@ -3,8 +3,13 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
+
 </head>
 <body>
 	<?php echo "hello world"; ?>
+	<footer>
+		<a href="#">Consulter les mentions légales</a>
+	</footer>
 </body>
+
 </html>
